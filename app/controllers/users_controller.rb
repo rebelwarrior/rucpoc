90153e408d86cd3b9659d9e8e94841f7ce0ec046
David Acevedo <rabbit.editor@gmail.com>
2013-08-15 15:36:10 -0400

    creates form and RESOURCES in routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  def show
+  end
+  
+  def create
+    @user = User.new(params[:user]) #not final implementatin
+    if @user.save
+      #handle successful save
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+  
 end
