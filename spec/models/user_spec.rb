736d67d588c362d0ac01eb9198a10538fda7255e
David Acevedo <rabbit.editor@gmail.com>
2013-08-21 11:26:15 -0400

    adds token creation and user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -91,5 +91,10 @@ describe User do
     it { should be_invalid }
   end
 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
 
 end
