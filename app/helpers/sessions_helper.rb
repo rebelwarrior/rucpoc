27e371547e9b066e67bb74549bdb600019f88b03
David Acevedo <rabbit.editor@gmail.com>
2013-08-21 13:02:58 -0400

    adds token fixes typos in controller

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,23 @@
 module SessionsHelper
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = { :value => remember_token, :expires => 2.days.from_now.utc }
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    self.current_user = user
+  end
+  
+  def signed_in?
+    not current_user.nil?
+  end
+  
+  def current_user=(user)
+    @current_user = user
+  end
+  
+  def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    #the remember token in the db is encrypted
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+  
 end
