91d69e1b02a4f03b0fd6f0b11def44bb4d2c569e
David Acevedo <rabbit.editor@gmail.com>
2013-08-21 15:43:24 -0400

    adds fastforward links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      #redirect_back_or in controllers/hepers/sessions_helper.rb
+      redirect_back_or user
     else
-      flash.now[:error] = "Email y/o password invalido"
+      flash.now[:error] = "Email y/o password inválido"
       render 'new'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user,  only: [:edit, :update]
+  before_action :correct_user,    only:[:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -17,9 +20,39 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    # now done w/ a before action
+    # @user = User.find(params[:id])
+  end
+  
+  def update
+    # @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Información del usuario actualizada."
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+    
+    # Before filters
+    
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please log in." unless signed_in?
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(login_url) unless current_user?(@user)
     end
   
 end
