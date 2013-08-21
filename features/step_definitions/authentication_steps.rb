4254c0998292b44d85e2a9d76a303feaf44019b2
David Acevedo <rabbit.editor@gmail.com>
2013-08-21 10:43:48 -0400

    adds step definition stubs

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,31 @@
+Given(/^a user visits the login page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^he submits invalid login information$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^he should see and error message$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a user visits the sigin page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the user has an account$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the user submits valid signin information$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^he should see his profile page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^he should see a logout link$/) do
+  pending # express the regexp above with the code you wish you had
+end
