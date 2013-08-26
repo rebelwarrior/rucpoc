f767405e48670743de757d5df8dfc71b4bc043a6
David Acevedo <rabbit.editor@gmail.com>
2013-08-26 15:28:19 -0400

    has bug editing collections but deleting works well

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -4,13 +4,13 @@ class CollectionsController < ApplicationController
   
   def new
     @user = current_user
+    redirect_to debtors_path if cookies[:current_debtor_id].nil?
     @debtor = Debtor.find_by_id(cookies[:current_debtor_id])
     @collection = Collection.new
   end
   
   def create
     @user = current_user
-    #need to create a current_debtor or something
     debtor = Debtor.find_by_id(cookies[:current_debtor_id])
     @collection = debtor.collections.build(collection_params)
     # @collection = Collection.new(collection_params) 
@@ -20,6 +20,27 @@ class CollectionsController < ApplicationController
     else
       flash[:error] = "Factura no gravada"
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = current_user
+    redirect_to debtors_path if cookies[:current_debtor_id].nil?
+    debtor = Debtor.find_by_id(cookies[:current_debtor_id])
+    @collection = debtor.collections.build(collection_params)
+  end
+  
+  def update
+    @user = current_user
+    debtor = Debtor.find_by_id(cookies[:current_debtor_id])
+    @collection = debtor.collections.build(collection_params)
+    # @collection = Collection.new(collection_params) 
+    if @collection.save
+      flash[:success] = "Factura Actualizada"
+      redirect_to @collection
+    else
+      flash[:error] = "Factura no actualizada"
+      render 'edit'
     end
   end
   
@@ -33,16 +54,18 @@ class CollectionsController < ApplicationController
     @collections = @debtor.collections
   end
   
-  # def update
-  #   @user = current_user
-  #   # @user = User.find(params[:id])
-  #   if @collection.update_attributes(debtor_params)
-  #     flash[:success] = "Informacion del deudor actualizada."
-  #     redirect_to @collection
-  #   else
-  #     render 'edit'
-  #   end
-  # end
+  def destroy
+    @user = current_user
+    if current_user.admin? or current_user.supervisor?
+      #Should only be certain users --> Supervisor users.
+      @debtor = Debtor.find_by_id(cookies[:current_debtor_id])
+      @debtor.collections.find(params[:id]).destroy
+      flash[:success] = "Factura borrada."
+      redirect_to collection_url
+    else
+      flash.now![:error] = "Solo Administradores y Supervisores pueden borrar la factura."
+    end
+  end
 
   
   private
@@ -55,5 +78,11 @@ class CollectionsController < ApplicationController
       end
     end
 
+    def current_debtor
+      unless cookies[:current_debtor_id].nil?
+        Debtor.find_by_id(cookies[:current_debtor_id])
+      end
+    end
+      
   
 end
