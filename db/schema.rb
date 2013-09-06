c7c34455790987ffc8cee29e1536e2b490a24081
David Acevedo <rabbit.editor@gmail.com>
2013-09-06 14:08:20 -0400

    adds supervisor trait in user model and editing to add it.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130822172547) do
+ActiveRecord::Schema.define(version: 20130905215113) do
 
   create_table "collections", force: true do |t|
     t.string   "internal_invoice_number"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20130822172547) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin",           default: false
+    t.boolean  "supervisor?",     default: false
+    t.boolean  "supervisor",      default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
